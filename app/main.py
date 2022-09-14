e71ce74eebf8fdaf4c189373e7129a8821690056
Daniel <mmdaniel@protonmail.com>
2022-09-14 11:03:03 +0100

    type fix

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
-from asyncio.log import logger
+import logging
 import os
 import re
 
-from telethon.sync import TelegramClient, events, utils
+from telethon.sync import TelegramClient, events
 from common import API_ID, API_HASH, CHANNEL_READ_ID, CHANNEL_SEND_ID, ALLOWED_MESSAGES
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 session = os.environ.get("TG_SESSION", "Forwader")
 client = TelegramClient(session, API_ID, API_HASH).start()
@@ -19,9 +21,11 @@ def main():
         async def handler(event):
             peer_channel = event.peer_id.stringify()
             try:
-                channel_id = re.findall(
-                    "[0-9]+(?!channel_id)", peer_channel, flags=re.IGNORECASE
-                )[0]
+                channel_id = int(
+                    re.findall(
+                        "[0-9]+(?!channel_id)", peer_channel, flags=re.IGNORECASE
+                    )[0]
+                )
             except:
                 channel_id = 0
 
@@ -37,7 +41,7 @@ def main():
 
             new_msg = msg + "\n**Leverage: 10x isolate**"
             logger.warning(f"Se ha enviado el siguiente mensaje: \n{new_msg}")
-            await client.send_message(CHANNEL_SEND_ID, new_msg)
+            await client.send_message(entity=CHANNEL_SEND_ID, message=new_msg)
 
         client.run_until_disconnected()
 
